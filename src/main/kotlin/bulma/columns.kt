13d65bfba2d00c3fa63ca007fe7d6f02ba49c890
Jean-Charles Roger <jeancharles.roger@me.com>
2020-06-01 18:05:17 +0200

    Adds support for columns offsets

--- src/main/kotlin/bulma/columns.kt
+++ src/main/kotlin/bulma/columns.kt
@@ -32,6 +32,32 @@ enum class ColumnSize(override val className: String) : HasClassName {
     S12("is-12")
 }
 
+enum class ColumnOffset(override val className: String) : HasClassName {
+    None(""),
+    ThreeQuarters("is-three-quarters"),
+    TwoThirds("is-two-thirds"),
+    Half("is-half"),
+    OneThird("is-one-third"),
+    OneQuarter("is-one-quarter"),
+    Full("is-full"),
+    FourFifths("is-four-fifths"),
+    ThreeFifths("is-three-fifths"),
+    TwoFifths("is-two-fifths"),
+    OneFifth("is-one-fifth"),
+    S1("is-1"),
+    S2("is-2"),
+    S3("is-3"),
+    S4("is-4"),
+    S5("is-5"),
+    S6("is-6"),
+    S7("is-7"),
+    S8("is-8"),
+    S9("is-9"),
+    S10("is-10"),
+    S11("is-11"),
+    S12("is-12")
+}
+
 enum class ColumnsGap(override val className: String) : HasClassName {
     None(""),
     S0("is-0"),
@@ -86,12 +112,14 @@ class Column(
     desktopSize: ColumnSize = ColumnSize.None,
     wideScreenSize: ColumnSize = ColumnSize.None,
     fullHdSize: ColumnSize = ColumnSize.None,
+    offset: ColumnOffset = ColumnOffset.None,
     narrow: Boolean = false
 ) : BulmaElement {
 
     override val root: HTMLElement = document.create.div("column")
 
-    // TODO support offset
+    /** Column [offset](https://bulma.io/documentation/columns/sizes/#offset). */
+    var offset by className(offset, root)
 
     /** [Size](https://bulma.io/documentation/columns/sizes) */
     var size by className(size, root)
